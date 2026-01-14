46b435d32211b26990dfdf83e4a18d753ade311d
Osman Onur KOÇ <37037217+osmanonurkoc@users.noreply.github.com>
2026-01-14 04:43:43 +0300

    version fix

--- compile.ps1
+++ compile.ps1
@@ -16,7 +16,7 @@ $Params = @{
     Company     = "Osman Onur Koç"
     Product     = "Windows Backup"
     Copyright   = "www.osmanonurkoc.com"
-    Version     = "1.0.0.0"
+    Version     = "11.4.0.0"
     NoConsole   = $true
     STA         = $true  # Critical for WPF UI (Single Threaded Apartment)
 }
